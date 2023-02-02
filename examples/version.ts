740d8ae5f92079be4752d5fcc41e04406ab721d4
RJ <rongjihuang@gmail.com>
2023-02-02 15:18:36 +0800

    Try another import map again

--- examples/version.ts
+++ examples/version.ts
@@ -1,4 +1,4 @@
-import { blue } from "deno.land/std/fmt/colors.ts"
+import { blue } from "https://deno.land/std/fmt/colors.ts"
 import { version } from "../mod.ts"
 
 console.log(`version ${blue(version)} is only for test publication`)
