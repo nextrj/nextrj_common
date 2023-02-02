f1d6099034c3e1e1922bead0a5956052fee95a84
RJ <rongjihuang@gmail.com>
2023-02-02 16:00:36 +0800

    Polishing

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 // ©2023 NextRJ Team. All Rights Reserved. MIT license.
 
+// deno standard library
 export { blue } from "https://deno.land/std@0.175.0/fmt/colors.ts"
 export { assert } from "https://deno.land/std@0.175.0/testing/asserts.ts"
