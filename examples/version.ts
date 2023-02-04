3dd74dae270c6da4ec81b9b8c7505de0d32e52ef
rj-stock <rongji_h@hotmail.com>
2023-02-04 11:55:14 +0800

    Try deno.jsonc/imports

--- examples/version.ts
+++ examples/version.ts
@@ -1,4 +1,7 @@
 import { blue } from "../deps.ts"
 import { version } from "../mod.ts"
+import { yellow } from "https://deno.land/std/fmt/colors.ts"
 
-console.log(`version ${blue(version)} is only for test publication`)
+console.log(
+  `${yellow("version")} ${blue(version)} is only for test publication`,
+)
